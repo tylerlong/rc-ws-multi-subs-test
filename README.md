915c798416c89a4ab6cb4ed9f02b1afcb630e3af
Tyler Long <tyler4long@gmail.com>
2023-10-19 13:34:07 -0700

    Add RingCentral

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Untitled App
+# RingCentral WebSocket multiple subscriptions test
+
+This is a test to check if a single RingCentral WebSocket can host multiple subscriptions.
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('Hello world!');
+import RingCentral from '@rc-ex/core';
+
+const rc = new RingCentral({
+  server: process.env.RINGCENTRAL_SERVER_URL,
+  clientId: process.env.RINGCENTRAL_CLIENT_ID,
+  clientSecret: process.env.RINGCENTRAL_CLIENT_SECRET,
+});
+
+const main = async () => {
+  await rc.authorize({ jwt: process.env.RINGCENTRAL_JWT_TOKEN! });
+  console.log(rc.token?.access_token);
+};
+
+main();
